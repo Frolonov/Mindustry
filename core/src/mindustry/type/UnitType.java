9c8754258ecf487e61788bb3c01e2026fb783552
Frolonov <81882506+Frolonov@users.noreply.github.com>
2025-11-10 19:38:31 -0500

    Update UnitType.java

--- core/src/mindustry/type/UnitType.java
+++ core/src/mindustry/type/UnitType.java
@@ -83,6 +83,8 @@ public class UnitType extends UnlockableContent implements Senseable{
     missileAccelTime = 0f,
     /** raw health amount */
     health = 200f,
+    /** real time health in percentage */
+    public float strength = 1f,
     /** incoming damage is reduced by this amount */
     armor = 0f,
     /** minimum range of any weapon; used for approaching targets. can be overridden by setting a value > 0. */
@@ -1067,6 +1069,11 @@ public class UnitType extends UnlockableContent implements Senseable{
 
         //only do this after everything else was initialized
         sample = constructor.get();
+    }
+
+    public float checkhealth(Unit unit){
+        float strength = Mathf.clamp(unit.healthf());
+        return strength;
     }
 
     public float estimateDps(){
